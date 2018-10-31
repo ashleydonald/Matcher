036d5e580b294003a037ee87653f9f8b5c57d4ae
RTOK AD <ashd32@gmail.com>
2018-10-31 19:50:39 -0400

    Replaced content

---
+++ public/js/applications.js
@@ -0,0 +1,49 @@
+$(function(){
+
+  /**
+   * Render the applications data to the appropriate part of the page.
+   * The first parameter is the array of applicationss to render.
+   * The second parameter is the parent element to append to (.requests or .applications).
+   */
+  const render = function (dataList, parent){
+    for (let i = 0; i < dataList.length; i++){
+      const applications = $('<div>').addClass('box');
+      applications.append(`<h3>${dataList[i].customerName}</h3>`);
+      applications.append(`<p>${dataList[i].customerEmail}</p>`);
+      applications.append(`<p>${dataList[i].phoneNumber}</p>`);
+
+      parent.append(applications)
+    }
+  }
+
+  /**
+   * GET all the requests from the server.
+   * Then call render to render the data.
+   */
+  const getRequests = function(){
+    $.ajax({
+      method: 'GET',
+      url: 'api/requests'
+    }).then(function(data){
+      render(data, $('.requests'));
+    });
+  }
+
+  /**
+   * GET all the applications data from the server.
+   * Then call render to render the data.
+   */
+  const getApplications = function(){
+    $.ajax({
+      method: 'GET',
+      url: 'api/applications'
+    }).then(function(data){
+      render(data, $('.applications'));
+    });
+  }
+  
+  getRequests();
+  getApplications();
+
+  $('#clear').on('click', clearAll);
+});
---
+++ routes/api-routes.js
@@ -0,0 +1,33 @@
+let requests = require('../data/requests.js');
+let applications = require('../data/applications.js');
+
+module.exports = function (app) {
+
+  /**
+   * GET the waitlist
+   */
+  app.get('/api/applications', function (req, res) {
+    res.json(applications);
+  });
+
+  /**
+   * GET the requests 
+   */
+  app.get('/api/requests', function (req, res) {
+    res.json(requests);
+  });
+
+  /**
+   * If there are fewer than 5 reservations, add the new reservation to the requests
+   * Otherwise add the new reservation to the waitlist
+   */
+  app.post('/api/requests', function (req, res) {
+    if (requests.length < 5) {
+      requests.push(req.body); 
+    } else {
+      applications.push(req.body);
+    }
+
+    res.end();
+  });
+}
