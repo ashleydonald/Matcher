53c373b95358eee85755d8636679251ed879f1a9
RTOK AD <ashd32@gmail.com>
2018-11-02 22:52:50 -0400

    fixed errors

--- public/js/applications.js
+++ public/js/applications.js
@@ -13,7 +13,7 @@ $(function () {
       customerName: $('#reserve-name').val().trim(),
       phoneNumber: $('#reserve-phone').val().trim(),
       customerEmail: $('#reserve-email').val().trim(),
-      customerID: $('#reserve-unique-id').val().trim()
+      serviceType: $("#serviceType :selected").val() 
     };
     console.log(newProvider);
     // Clear the form when submitting
@@ -21,6 +21,8 @@ $(function () {
     $('#reserve-phone').val('');
     $('#reserve-email').val('');
     $('#reserve-unique-id').val('');
+
+    console.log(newProvider);
 
     $.ajax({
       method: 'POST',
@@ -33,69 +35,17 @@ $(function () {
           method: 'GET',
           url: 'api/requests'
 
-        }) // AP: MOVE THESE TWO PARENTHESES BACK UP HERE AGAIN
+        }); 
       }).then(function (response) {
-        // console.log(response);
-        var serviceType = newProvider.customerID;
+              
         console.log(response[serviceType]);
-      })
-    // }) AP: WE MOVED THESE TWO DOWN HERE TO DEBUG BUT TURNS OUT IT CAN STAY IN LINE 36
+      });
+   
 
-  } // AP: MOVED CURLY BRACE FROM LINE 46 TO UP HERE IN LINE 44
-    $('.submit').on('click', addProvider());
-  // } AP: DON't NEED CURLY BRACE HERE
+  }; 
+    $('.submit').on('click', addProvider);
+ 
 });
 
 
 
-
-// $(function(){
-
-//   /**
-//    * Render the applications data to the appropriate part of the page.
-//    * The first parameter is the array of applicationss to render.
-//    * The second parameter is the parent element to append to (.requests or .applications).
-//    */
-//   const render = function (dataList, parent){
-//     for (let i = 0; i < dataList.length; i++){
-//       const applications = $('<div>').addClass('box');
-//       applications.append(`<h3>${dataList[i].customerName}</h3>`);
-//       applications.append(`<p>${dataList[i].customerEmail}</p>`);
-//       applications.append(`<p>${dataList[i].phoneNumber}</p>`);
-
-//       parent.append(applications)
-//     }
-//   }
-
-//   /**
-//    * GET all the requests from the server.
-//    * Then call render to render the data.
-//    */
-//   const getRequests = function(){
-//     $.ajax({
-//       method: 'GET',
-//       url: 'api/requests'
-//     }).then(function(data){
-//       console.log(data);
-//       render(data, $('.requests'));
-//     });
-//   }
-
-//   /**
-//    * GET all the applications data from the server.
-//    * Then call render to render the data.
-//    */
-//   const getApplications = function(){
-//     $.ajax({
-//       method: 'GET',
-//       url: 'api/applications'
-//     }).then(function(data){
-//       render(data, $('.applications'));
-//     });
-//   }
-  
-//   getRequests();
-//   getApplications();
-
-//   // $('#clear').on('click', clearAll);
-// });
--- public/js/requests.js
+++ public/js/requests.js
@@ -13,9 +13,10 @@ $(function () {
       customerName: $('#reserve-name').val().trim(),
       phoneNumber: $('#reserve-phone').val().trim(),
       customerEmail: $('#reserve-email').val().trim(),
-      customerID: $('#reserve-unique-id').val().trim()
+      serviceType: $("#serviceType :selected").val() 
     };
     console.log(newRequest);
+
     // Clear the form when submitting
     $('#reserve-name').val('');
     $('#reserve-phone').val('');
@@ -28,21 +29,20 @@ $(function () {
       data: newRequest
     })
       .then(function () {
-        console.log("functions");
         return $.ajax({
           method: 'GET',
           url: 'api/applications'
 
-        }) // AP: MOVE THESE TWO PARENTHESES BACK UP HERE AGAIN
-      }).then(function (response) {
-        // console.log(response);
-        var serviceType = newRequest.customerID;
-        console.log(response[serviceType]);
-        return(serviceType);
-      })
-    // }) AP: WE MOVED THESE TWO DOWN HERE TO DEBUG BUT TURNS OUT IT CAN STAY IN LINE 36
+        });
+      }).then(function (serviceType) {
 
-  } // AP: MOVED CURLY BRACE FROM LINE 46 TO UP HERE IN LINE 44
-    $('.submit').on('click', addRequest)
-  // } AP: DON't NEED CURLY BRACE HERE
+   
+        console.log(newRequest.serviceType);
+        return (newRequest.serviceType);
+
+      });
+
+  };
+  $('.submit').on('click', addRequest);
+
 });
