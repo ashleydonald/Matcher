2b6e7a28f861851e730980b00b6f2b1abbebb48a
RTOK AD <ashd32@gmail.com>
2018-11-03 23:57:12 -0400

    fixed request and provider objects

--- public/js/providerApplication.js
+++ public/js/providerApplication.js
@@ -15,33 +15,71 @@ $(function () {
       customerEmail: $('#reserve-email').val().trim(),
       serviceType: $("#serviceType :selected").val()
     };
+    console.log(newProvider);
 
     // Clear the form when submitting
     $('#reserve-name').val('');
     $('#reserve-phone').val('');
     $('#reserve-email').val('');
-    $("#serviceType :selected").val();
+    $('#reserve-unique-id').val('');
 
     $.ajax({
       method: 'POST',
-      url: 'api/applications',
+      url: 'api/providerList',
       data: newProvider
     })
       .then(function () {
         return $.ajax({
           method: 'GET',
-          url: 'api/applications'
+          url: 'api/providerList'
 
         });
       }).then(function (serviceType) {
 
 
         console.log(newProvider.serviceType);
-        // return (newProvider);
+        console.log(newProvider);
 
       });
 
   };
   $('.submit').on('click', addProvider);
 
+});
+
+$(function () {
+
+  /**
+   * Render the providerApplication data to the appropriate part of the page.
+   * The first parameter is the array of providerApplications to render.
+   * The second parameter is the parent element to append to (.providerList or .waitlist).
+   */
+  const render = function (dataList, parent) {
+    for (let i = 0; i < dataList.length; i++) {
+      const applications = $('<div>').addClass('box');
+      applications.append(`<h3>${dataList[i].customerName}</h3>`);
+      applications.append(`<p>${dataList[i].customerEmail}</p>`);
+      applications.append(`<p>${dataList[i].phoneNumber}</p>`);
+      applications.append(`<p>${dataList[i].serviceType}</p>`);
+
+      parent.append(applications)
+    }
+  }
+
+  /**
+   * GET all the providerList from the server.
+   * Then call render to render the data.
+   */
+  const getProviderList = function () {
+    $.ajax({
+      method: 'GET',
+      url: 'api/providerList'
+    }).then(function (data) {
+      render(data, $('.providerList'));
+    });
+  }
+
+  getProviderList();
+
+  $('#clear').on('click', render);
 });
--- public/js/requests.js
+++ public/js/requests.js
@@ -1,98 +1,86 @@
 $(function () {
 
-    /**
-     * Get the form info from the page.
-     * Clear the form. 
-     * Send the data in a POST request.
-     */
-    const addRequest = function (event) {
-      event.preventDefault();
-  
-      // Here we grab the form elements
-      const newRequest = {
-        customerName: $('#reserve-name').val().trim(),
-        phoneNumber: $('#reserve-phone').val().trim(),
-        customerEmail: $('#reserve-email').val().trim(),
-        serviceType: $("#serviceType :selected").val() 
-      };
-      console.log(newRequest);
-  
-      // Clear the form when submitting
-      $('#reserve-name').val('');
-      $('#reserve-phone').val('');
-      $('#reserve-email').val('');
-      $('#reserve-unique-id').val('');
-  
-      $.ajax({
-        method: 'POST',
-        url: 'api/requestList',
-        data: newRequest
-      })
-        .then(function () {
-          return $.ajax({
-            method: 'GET',
-            url: 'api/requestList'
-  
-          });
-        }).then(function (serviceType) {
-  
-     
-          console.log(newRequest.serviceType);
-          console.log(newRequest);
-  
-        });
-  
-    };
-    $('.submit').on('click', addRequest);
-  
-  });
-  
-  $(function(){
+  /**
+   * Get the form info from the page.
+   * Clear the form. 
+   * Send the data in a POST request.
+   */
+  const addRequest = function (event) {
+    event.preventDefault();
 
-    /**
-     * Render the providerApplication data to the appropriate part of the page.
-     * The first parameter is the array of providerApplications to render.
-     * The second parameter is the parent element to append to (.providerList or .waitlist).
-     */
-    const render = function (dataList, parent){
-      for (let i = 0; i < dataList.length; i++){
-        const applications = $('<div>').addClass('box');
-        applications.append(`<h3>${dataList[i].customerName}</h3>`);
-        applications.append(`<p>${dataList[i].customerEmail}</p>`);
-        applications.append(`<p>${dataList[i].phoneNumber}</p>`);
-  
-        parent.append(applications)
-      }
-    }
-  
-    /**
-     * GET all the providerList from the server.
-     * Then call render to render the data.
-     */
-    const getProviderList = function(){
-      $.ajax({
-        method: 'GET',
-        url: 'api/providerList'
-      }).then(function(data){
-        render(data, $('.providerList'));
+    // Here we grab the form elements
+    const newRequest = {
+      customerName: $('#reserve-name').val().trim(),
+      phoneNumber: $('#reserve-phone').val().trim(),
+      customerEmail: $('#reserve-email').val().trim(),
+      serviceType: $("#serviceType :selected").val()
+    };
+    console.log(newRequest);
+
+    // Clear the form when submitting
+    $('#reserve-name').val('');
+    $('#reserve-phone').val('');
+    $('#reserve-email').val('');
+    $('#reserve-unique-id').val('');
+
+    $.ajax({
+      method: 'POST',
+      url: 'api/requestList',
+      data: newRequest
+    })
+      .then(function () {
+        return $.ajax({
+          method: 'GET',
+          url: 'api/requestList'
+
+        });
+      }).then(function (serviceType) {
+
+
+        console.log(newRequest.serviceType);
+        console.log(newRequest);
+
       });
+
+  };
+  $('.submit').on('click', addRequest);
+
+});
+
+$(function () {
+
+  /**
+   * Render the providerApplication data to the appropriate part of the page.
+   * The first parameter is the array of providerApplications to render.
+   * The second parameter is the parent element to append to (.providerList or .waitlist).
+   */
+  const render = function (dataList, parent) {
+    for (let i = 0; i < dataList.length; i++) {
+      const applications = $('<div>').addClass('box');
+      applications.append(`<h3>${dataList[i].customerName}</h3>`);
+      applications.append(`<p>${dataList[i].customerEmail}</p>`);
+      applications.append(`<p>${dataList[i].phoneNumber}</p>`);
+      applications.append(`<p>${dataList[i].serviceType}</p>`);
+
+      parent.append(applications)
     }
-  
-    /**
-     * GET all the waitlist data from the server.
-     * Then call render to render the data.
-     */
-    const getRequestList = function(){
-      $.ajax({
-        method: 'GET',
-        url: 'api/requestList'
-      }).then(function(data){
-        render(data, $('.requestlist'));
-      });
-    }
-    
-    getProviderList();
-    getRequestList();
-  
-    $('#clear').on('click', render);
-  });
+  }
+
+
+  /**
+   * GET all the waitlist data from the server.
+   * Then call render to render the data.
+   */
+  const getRequestList = function () {
+    $.ajax({
+      method: 'GET',
+      url: 'api/requestList'
+    }).then(function (data) {
+      render(data, $('.requestlist'));
+    });
+  }
+
+  getRequestList();
+
+  $('#clear').on('click', render);
+});
