80cba10f7aa6a2775a5b082b4e55e5d77e3232dc
ashd32 <ashd32@gmail.com>
2018-11-25 14:05:45 -0500

    made images responsive

--- data/providerList.js
+++ data/providerList.js
@@ -4,35 +4,36 @@ var providerList = [
      customerEmail: "urieltheplumber@gmail.com",
      phoneNumber: "404-030-5300",
      serviceType: "plumbing",
-     image: "<img src='../images/plumber.jpg'></img>"
+     image: "<img class='img-responsive' src='../images/plumber.jpg' style='max-height:250px;'>"
+
     },
     {
      customerName: "Mario",
      customerEmail: "mariotheelectrician@gmail.com",
      phoneNumber: "404-530-3893",
      serviceType: "electrical",
-     image: "<img src='../images/electrician.jpg'></img>"
+     image: "<img class='img-responsive' src='../images/electrician.jpg' style='max-height:150px;'>"
     },
     {   
      customerName: "Angel",
      customerEmail: "angelthepainter@gmail.com",
      phoneNumber: "678-356-3634",
      serviceType: "painting",
-     image: "<img src='../images/painter.jpg'></img>"
+     image: "<img class='img-responsive' src='../images/painter.jpg' style='max-height:250px;'>"
     },
     {
      customerName: "Tommy",
      customerEmail: "tommysHVAC@gmail.com",
      phoneNumber: "706-227-3564",
      serviceType: "HVAC",
-     image: "<img src='../images/hvac.jpg'></img>"
+     image: "<img class='img-responsive' src='../images/hvac.jpg' style='max-width:250px;'>"
     },
     {
      customerName: "Breona",
      customerEmail: "bresContracting@gmail.com",
      phoneNumber: "404-985-2753",
      serviceType: "general_contracting",
-     image: "<img src='../images/contractor.jpg'></img>"
+     image: "<img class='img-responsive' src='../images/contractor.jpg' style='max-width:250px;'>"
     }
 
 ];
